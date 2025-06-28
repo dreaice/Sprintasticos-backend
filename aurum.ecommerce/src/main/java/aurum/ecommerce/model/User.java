0e31e2dc64c024356602becf6244707aba3511af
Oswaldo Ramirez <73424112+Oswaldoramirezb@users.noreply.github.com>
2025-06-28 01:58:32 -0600

    Quitar import que no funciona

--- aurum.ecommerce/src/main/java/aurum/ecommerce/model/User.java
+++ aurum.ecommerce/src/main/java/aurum/ecommerce/model/User.java
@@ -3,8 +3,6 @@ package aurum.ecommerce.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
